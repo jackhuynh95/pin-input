ceb94ee9b1fdd7f7ac5b2eeb2bbc2c5b0b0da6ef
Jack Huynh <kietdien88@gmail.com>
2024-03-19 17:22:41 +0700

    feat: component test

--- test/pincode.test.ts
+++ test/pincode.test.ts
@@ -3,19 +3,23 @@ import { describe, expect, it } from 'vitest'
 import ThePINInput from '../src/components/ThePINInput.vue'
 
 describe('component ThePINInput.vue', () => {
-  it('should render', async () => {
+  it('renders the correct number of pin code cells', async () => {
     const wrapper = mount(ThePINInput, { props: { length: 5, modelValue: '12345' } })
 
     await nextTick()
 
-    expect(wrapper.findAll('input[type="tel"]').length).toBe(5)
+    const cellCount = wrapper.findAll('input').length
+    expect(cellCount).toBe(5)
   })
 
-  it('should be interactive', async () => {
+  it('displays the correct value when pin code is provided', async () => {
     const wrapper = mount(ThePINInput, { props: { length: 5, modelValue: '12345' } })
 
     await nextTick()
 
-    expect(wrapper.findAll('input[type="tel"]').length).toBe(5)
+    const inputValues = wrapper.findAll('input').map(input => input.element.value)
+    const expectedValues = ['1', '2', '3', '4', '5']
+
+    expect(inputValues).toEqual(expectedValues)
   })
 })
